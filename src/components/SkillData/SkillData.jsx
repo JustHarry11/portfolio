f402d99f44324afff6dfef84559d4aeb4d6755dc
Harry <harrylippa@gmail.com>
2025-07-07 11:50:13 +0100

    Skills page added

---
+++ src/components/SkillData/SkillData.jsx
@@ -0,0 +1,60 @@
+const skills = [
+    {
+        name: "JavaScript",
+        image: "/images/JavaScript-logo.png"
+    },
+    {
+        name: "HTML5",
+        image: "/images/HTML5-logo.png"
+    },
+    {
+        name: "CSS",
+        image: "/images/CSS-logo.png"
+    },
+    {
+        name: "Python",
+        image: "/images/Python-logo.png"
+    },
+    {
+        name: "Express.js",
+        image: "/images/Expressjs-logo.png"
+    },
+    {
+        name: "React.js",
+        image: "/images/React-logo.png"
+    },
+    {
+        name: "Node.js",
+        image: "/images/Node-logo.png"
+    },
+    {
+        name: "MongoDB",
+        image: "/images/MongoDB.png"
+    },
+    {
+        name: "RESTful Routing",
+        image: "/images/REST-logo.png"
+    },
+    {
+        name: "JSON",
+        image: "/images/JSON-logo.png"
+    },
+    {
+        name: "Postman",
+        image: "/images/Postman-logo.png"
+    },
+    {
+        name: "GitHub",
+        image: "/images/GitHub-logo.png"
+    },
+    {
+        name: "VSCode",
+        image: "/images/VS-logo.png"
+    },
+    {
+        name: "Trello",
+        image: "/images/Trello-logo.png"
+    },
+]
+
+export default skills;
--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,7 +1,18 @@
 import './Skills.css';
-
-export default function Skills(){
+import skills from '../SkillData/SkillData'
+export default function Skills() {
     return (
-        <h1 className="skill-name-title">Skills</h1>
+        <>
+            <h1 className="skill-name-title">Skills</h1>
+            <div className='skills-grid'>
+                {skills.map((skill, idx) => (
+                    <div key={idx} className="skill-card">
+                        <h3>{skill.name}</h3>
+                        <img src={skill.image} alt={skill.name} className='skill-img'/>
+                    </div>
+                ))}
+            </div>
+        </>
+
     )
 }
