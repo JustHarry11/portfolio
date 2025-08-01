bb3f4b15eb8dffe5f0b0f0cec86b764504857659
Harry <harrylippa@gmail.com>
2025-08-01 13:50:12 +0100

    Changed the position of the title of each page

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -8,45 +8,49 @@ import { Navigation } from 'swiper/modules';
 
 export default function Projects() {
   return (
-    <div className="projects-container">
+    <>
       <h1 className='project-name-title'>Projects</h1>
-      <Swiper
-        modules={[Navigation]}
-        spaceBetween={50}
-        slidesPerView={1}
-        navigation
-        centeredSlides
-        loop
-        className="project-swiper"
-      >
-        {projects.map((project, idx) => (
-          <SwiperSlide key={idx}>
-            <div className="project-card">
-              <div className='project-content'>
-                <div className='project-image-wrapper'>
-                  <img src={project.image} alt={project.name} className='project-image' />
-                </div>
-                <div className='project-text'>
-                  <h3>{project.name}</h3>
-                  <p>{project.description}</p>
-                  <p><strong>Tech:</strong> {project.tech.join(", ")}</p>
-                  <a href={project.githubfront} target="_blank" rel="noopener noreferrer">GitHub Front-End</a> |{" "}
-                  {project.githubback && (
-                    <>
-                      <a href={project.githubback} target="_blank" rel="noopener noreferrer">
-                        GitHub Back-End
-                      </a>
-                      {" | "}
-                    </>
-                  )}
-                  <a href={project.demo} target="_blank" rel="noopener noreferrer">Live Site</a>
-                </div>
+      <div className="projects-container">
 
+        <Swiper
+          modules={[Navigation]}
+          spaceBetween={50}
+          slidesPerView={1}
+          navigation
+          centeredSlides
+          loop
+          className="project-swiper"
+        >
+          {projects.map((project, idx) => (
+            <SwiperSlide key={idx}>
+              <div className="project-card">
+                <div className='project-content'>
+                  <div className='project-image-wrapper'>
+                    <img src={project.image} alt={project.name} className='project-image' />
+                  </div>
+                  <div className='project-text'>
+                    <h3>{project.name}</h3>
+                    <p>{project.description}</p>
+                    <p><strong>Tech:</strong> {project.tech.join(", ")}</p>
+                    <a href={project.githubfront} target="_blank" rel="noopener noreferrer">GitHub Front-End</a> |{" "}
+                    {project.githubback && (
+                      <>
+                        <a href={project.githubback} target="_blank" rel="noopener noreferrer">
+                          GitHub Back-End
+                        </a>
+                        {" | "}
+                      </>
+                    )}
+                    <a href={project.demo} target="_blank" rel="noopener noreferrer">Live Site</a>
+                  </div>
+
+                </div>
               </div>
-            </div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
-    </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </>
+
   );
 }
