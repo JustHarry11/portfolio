22e9e15186fd34e8c9703ea67a0a435fe45ad034
Harry <harrylippa@gmail.com>
2025-08-04 13:04:52 +0100

    Changes to the contact page

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -7,17 +7,19 @@ export default function Contact() {
             <div className='contact-detail'>
                 <img src="/images/Me.png" alt='me' className='contact-img' />
 
-                <p>I have always enjoyed problem solving and understanding how things work. I studied Game Design at BA level and Software Engineering at a bootcamp from General Assembly. Both courses have enabled me to navigate and figure out solutions. Similarities in both courses have helped me apply my skills effectively.</p>
-
-                <a href="https://github.com/JustHarry11" target="_blank" rel="noopener noreferrer" className="contact-item">
-                    <img src="/images/GitHub-logo.png" alt="GitHub" className="icon" />
-                    <h4>GitHub</h4>
-                </a>
-                <a href="https://www.linkedin.com/in/harry-lippa/" target="_blank" rel="noopener noreferrer" className="contact-item">
-                    <img src="/images/LinkedIn-logo.png" alt="LinkedIn" className="icon" />
-                    <h4>LinkedIn</h4>
-                </a>
-
+                <div className='contact-text'>
+                    <p>I have always enjoyed problem solving and understanding how things work. I studied Game Design at BA level and Software Engineering at a bootcamp from General Assembly. Both courses have enabled me to navigate and figure out solutions. Similarities in both courses have helped me apply my skills effectively.</p>
+                    <div className='contact-links'>
+                        <a href="https://github.com/JustHarry11" target="_blank" rel="noopener noreferrer" className="contact-item">
+                            <img src="/images/GitHub-logo.png" alt="GitHub" className="icon" />
+                            <h4>GitHub</h4>
+                        </a>
+                        <a href="https://www.linkedin.com/in/harry-lippa/" target="_blank" rel="noopener noreferrer" className="contact-item">
+                            <img src="/images/LinkedIn-logo.png" alt="LinkedIn" className="icon" />
+                            <h4>LinkedIn</h4>
+                        </a>
+                    </div>
+                </div>
             </div>
         </>
 
